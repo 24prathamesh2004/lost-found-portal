2e0cc9d73480398f87ef5352806320962d41846d
Prathamesh Bhavsar <158708757+24prathamesh2004@users.noreply.github.com>
2025-04-20 13:37:36 +0530

    Delete client directory

--- client/src/components/config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-
- const baseURL = 'http://localhost:5000';
-//const baseURL = "https://lostandfoundbackend-y9qs.onrender.com";
-const config = {
-  baseURL,
-};
-
-export default config;
