05d1055d6580c6fe69bf8d63f7f1e2178d7036bb
Prathamesh Bhavsar <158708757+24prathamesh2004@users.noreply.github.com>
2025-10-04 12:16:01 +0530

    Update GroupInfo.js

--- client/src/components/GroupInfo.js
+++ client/src/components/GroupInfo.js
@@ -29,35 +29,12 @@ const GroupInfo = () => {
       <h2 style={groupTitleStyle}>Group 10 Members:</h2>
       <ul style={memberListStyle}>
         <li style={memberItemStyle}>
-          Ankesh Kumar (B210821CS) -{" "}
-          <a href="mailto:ankesh_b2100821cs@nitc.ac.in">
-            ankesh_b210821cs@nitc.ac.in
+          Prathamesh Bhavsar (UCS22M1019) -{" "}
+          <a href="mailto:24prathamesh2004@gmail.com">
+            24prathamesh2004@gmail.com
           </a>
         </li>
-        <li style={memberItemStyle}>
-          Pranav Prashant (B210460CS) -{" "}
-          <a href="mailto:pranav_b210460cs@nitc.ac.in">
-            pranav_b210460cs@nitc.ac.in
-          </a>
-        </li>
-        <li style={memberItemStyle}>
-          Rahul B Menon (B210482CS) -{" "}
-          <a href="mailto:rahul_b210482cs@nitc.ac.in">
-            rahul_b210482cs@nitc.ac.in
-          </a>
-        </li>
-        <li style={memberItemStyle}>
-          Rahul P Aroli (B210544CS) -{" "}
-          <a href="mailto:rahul_b210544cs@nitc.ac.in">
-            rahul_b210544cs@nitc.ac.in
-          </a>
-        </li>
-        <li style={memberItemStyle}>
-          S Rishi Mohan (B210552CS) -{" "}
-          <a href="mailto:rishi_b210552cs@nitc.ac.in">
-            rishi_b210552cs@nitc.ac.in
-          </a>
-        </li>
+        
       </ul>
     </div>
   );
